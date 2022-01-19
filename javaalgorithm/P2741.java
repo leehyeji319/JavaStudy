c93f9a67f320ec79e0b77ca27d6048b55e0e7849
Hyeji Lee <zzambbang@gmail.com>
2022-01-19 23:43:50 +0900

    java algorithm basic

---
+++ javaalgorithm/P2741.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P2741 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i < a + 1; i ++) {
+			System.out.println(i);
+		}
+	}
+}
