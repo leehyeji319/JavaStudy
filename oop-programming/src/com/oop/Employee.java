d63aeff153b67c06f83f3620986151d144ac9458
zzambbang <zzambbang@gmail.com>
2022-07-21 11:03:53 +0900

    feat: 폴더정리

---
+++ oop-programming/src/com/oop/Employee.java
@@ -0,0 +1,21 @@
+package com.oop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyNpc {
+	private MyNpc() {
+
+	}
+	public static void update(int x) {
+		x = x + 10;
+	}
+
+	// public static void main(String[] args) {
+	//
+	// }
+}
+
+class No {
+	MyNpc.update();
+}
